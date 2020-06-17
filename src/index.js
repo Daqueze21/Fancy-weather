720897660e3a67bf3781af79f5cdb1f7b680e9ff
Dzmitry Bryt <bdv212@yandex.ru>
2020-06-17 18:42:19 +0300

    feat: create cityInfo module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import UserGeolocation from './modules/userGeolocation';
+import CityInfo from './modules/cityInfo';
 
 import './scss/index.scss'; // scss
 
@@ -10,6 +11,27 @@ window.addEventListener('load', () => {
     .getUserGeolocation()
     .then((city) => {
       console.log('geoloc', city);
+
+      const cityInfo = new CityInfo();
+
+      cityInfo
+        .getCityInfo(city, languageSelected.value.toLowerCase())
+        .then(result => {
+          
+
+          console.log(result)
+
+
+        })
+        .catch(err => console.log('cityinfo', err));
+
+      localStorage.setItem('userCity', city);
     })
     .catch((err) => console.log('geolocation', err));
 });
+
+
+/** change language block */
+const languageSelected = document.querySelector('.language select');
+
+/** change language block END */
---
+++ src/modules/cityInfo.js
@@ -0,0 +1,17 @@
+export default class CityInfo {
+  constructor() {
+    // api data
+    this.key = '9840e44de32341028d4fa0189698d503';
+    this.url = 'https://api.opencagedata.com/geocode/v1/json?q=';
+  }
+
+  // fetch city data from API
+  async getCityInfo(city, lang) {
+    const response = await fetch(
+      `${this.url}${city}&key=${this.key}&language=${lang}&pretty=1&no_annotations=1`
+    );
+    const data = await response.json();
+
+    return data;
+  }
+}
