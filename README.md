18183c3d74c57f236d37333b5ac434b536cdec9f
Dzmitry Bryt <bdv212@yandex.ru>
2020-06-17 23:00:30 +0300

    feat: update readmi.md, build project

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Fancy-weather
+
+## Task
+```
+https://github.com/rolling-scopes-school/tasks/blob/master/tasks/fancy-weather.md
+```
+
+## Project setup
+```
+npm install
+```
+
+### Compiles for development
+```
+npm run dev
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Dev server  mode   
+```
+npm run start
+```
--- src/modules/userGeolocation.js
+++ src/modules/userGeolocation.js
@@ -12,31 +12,3 @@ export default class UserGeolocation {
     return data.city;
   }
 }
-/** render user city weather data END */
-
-/** Voice recognition block */
-const talkBtn = document.querySelector('.talkBtn');
-const Input = document.querySelector('.search-input');
-
-const SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
-
-recognition.onstart = () => {
-  console.log('voice recognition');
-};
-
-recognition.onresult = (event) => {
-  const current = event.resultIndex;
-  const { transcript } = event.results[current][0];
-  saveRecognitionToInput(transcript);
-};
-
-const saveRecognitionToInput = (transcript) => {
-  Input.value = transcript;
-};
-
-talkBtn.addEventListener('click', () => {
-  recognition.start();
-});
-/** Voice recognition block END */
