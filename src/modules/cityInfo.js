5f7c715b50312f31d96dee6d5088a858a70c5354
Dzmitry Bryt <bdv212@yandex.ru>
2020-06-20 17:46:16 +0300

    feat: remove key

--- src/modules/cityInfo.js
+++ src/modules/cityInfo.js
@@ -1,7 +1,7 @@
 export default class CityInfo {
   constructor() {
     // api data
-    this.key = '9840e44de32341028d4fa0189698d503';
+
     this.url = 'https://api.opencagedata.com/geocode/v1/json?q=';
   }
 
